008b3d46a52830cf24ad8d420d5dbdac3f93eda8
meri sharashenidze <merisharashenidze@gmail.com>
2023-06-15 21:17:43 +0400

    price-counter

--- product-dictionary.py
+++ product-dictionary.py
@@ -15,3 +15,6 @@ values=product_list
 dictionary=dict(zip(keys,values))
 print(f"the most expensive product is : {dictionary[max(price_list)]}")
 print(f"the cheapest product is: {dictionary[min(price_list)]}")
+for i in dictionary:
+    if price_list.count(i)>1:
+        print(f"there are more than one with same prices ,for example :{dictionary[i]}")  
